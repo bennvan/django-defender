aa64945ad78455ffc42e2664f040e4710fe61ac5
bennvan <44966500+bennvan@users.noreply.github.com>
2020-03-31 03:37:39 +1100

    check permission for views

--- defender/views.py
+++ defender/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib import admin
 
 try:
     from django.urls import reverse
@@ -10,20 +11,21 @@ except ImportError:
 from .utils import get_blocked_ips, get_blocked_usernames, unblock_ip, unblock_username
 
 
-@staff_member_required
+@user_passes_test(lambda u: u.has_perm('defender.change_accessattempt'))
 def block_view(request):
+    context = admin.site.each_context(request)
     """ List the blocked IP and Usernames """
     blocked_ip_list = get_blocked_ips()
     blocked_username_list = get_blocked_usernames()
 
-    context = {
+    context.update({
         "blocked_ip_list": blocked_ip_list,
         "blocked_username_list": blocked_username_list,
-    }
+    })
     return render(request, "defender/admin/blocks.html", context)
 
 
-@staff_member_required
+@user_passes_test(lambda u: u.has_perm('defender.change_accessattempt'))
 def unblock_ip_view(request, ip_address):
     """ upblock the given ip """
     if request.method == "POST":
@@ -31,7 +33,7 @@ def unblock_ip_view(request, ip_address):
     return HttpResponseRedirect(reverse("defender_blocks_view"))
 
 
-@staff_member_required
+@user_passes_test(lambda u: u.has_perm('defender.change_accessattempt'))
 def unblock_username_view(request, username):
     """ unblockt he given username """
     if request.method == "POST":
